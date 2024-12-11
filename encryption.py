31f7e6f7b823803580ace21aa41065a3e93e6ea5
R-P17 <49292213+R-P17@users.noreply.github.com>
2024-12-12 01:12:48 +0200

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,313 +1,56 @@
 # -*- coding: utf-8 -*-
-import sys
 import time
-print "For encryption press 'e' "
+
+ENCRYPT_DICT = {
+    'a': 'cda', 'b': 'fba', 'c': 'dvn', 'd': 'hxw', 'e': 'kel', 'f': 'wcg', 'g': 'qjr',
+    'h': 'opz', 'i': 'msa', 'j': 'erb', 'k': 'cui', 'l': 'dot', 'm': 'rpe', 'n': 'uyx',
+    'o': 'ncz', 'p': 'tfm', 'q': 'htq', 'r': 'hlp', 's': 'wzf', 't': 'zua', 'u': 'bak',
+    'v': 'drk', 'w': 'she', 'x': 'foq', 'y': 'dor', 'z': 'imd', 'A': 'cDa', 'B': 'FbA',
+    'C': 'Dvn', 'D': 'hxW', 'E': 'Kel', 'F': 'wCG', 'G': 'QJr', 'H': 'OPZ', 'I': 'MsA',
+    'J': 'eRb', 'K': 'CUi', 'L': 'DOT', 'M': 'RPe', 'N': 'Uyx', 'O': 'Ncz', 'P': 'TFm',
+    'Q': 'htQ', 'R': 'hLP', 'S': 'wZf', 'T': 'ZUA', 'U': 'BaK', 'V': 'dRk', 'W': 'She',
+    'X': 'FOq', 'Y': 'doR', 'Z': 'IMD', ' ': '9sb', '!': '..@', '@': '[&8.', '#': '3[]',
+    '$': '*g*', '%': '&qX', '^': '000', '&': 'AAA', '*': '5d:', '(': 'z11', ')': '235',
+    '-': 'n4-', '_': '9n3', '=': '-=-', '+': '#i#', '[': '(s)', '{': '888', ']': '456',
+    '}': '8gh', ';': 'p.,', ':': '^r*', "'": '$3d', '"': '+Zl', '|': 'J.&', '<': '5U{',
+    ',': '_S,', '>': '!8+', '.': 'T??', '/': 'Z^^', '?': ';R;', '0': '2zz', '1': '3Tc',
+    '2': '4z0', '3': '5-R', '4': '6sW', '5': '7PR', '6': '89F', '7': '997', '8': '10d',
+    '9': '11J'
+}
+
+DECRYPT_DICT = {v: k for k, v in ENCRYPT_DICT.items()}
+def encrypt(text):
+    result = []
+    for char in text:
+        result.append(ENCRYPT_DICT.get(char, char))
+    return ''.join(result)
+
+def decrypt(text):
+    result = []
+    temp = ""
+    for char in text:
+        temp += char
+        if temp in DECRYPT_DICT:
+            result.append(DECRYPT_DICT[temp])
+            temp = ""
+    return ''.join(result)
+print("Για κρυπτογράφηση πατήστε 'e'")
 time.sleep(1)
-print "For decryption press 'd' "
-encryption=raw_input()
-while (encryption!="e" and encryption!="E" and encryption!="d" and encryption!="D"):
-    print "Wrong input"
-    print "For encryption press 'e'"
-    time.sleep(1)
-    print "For decryption press 'd'"
-    encryption=raw_input()
-else:
-    if (encryption=="e" or encryption=="E"):
-        print "type the text for encryption"
-        encryption_text=raw_input()
-        for i in encryption_text:
-            if(i=="a"):
-                text=i.replace('a','cda')
-                sys.stdout.write(text)
-            if(i=="\u03A8"):
-                text=i.replace('\u03A8', 'cbxs')
-                sys.stdout.write(text)
-            if(i=="b"):
-                text=i.replace('b','fba')
-                sys.stdout.write(text)
-            if(i=="c"):
-               text=i.replace('c','dvn')
-               sys.stdout.write(text)
-            if(i=="d"):
-                text=i.replace('d', 'hxw')
-                sys.stdout.write(text)
-            if(i=="b"):
-                text=i.replace('b', 'fba')
-                sys.stdout.write(text)
-            if(i=="c"):
-                text=i.replace('c', 'dvn')
-                sys.stdout.write(text)
-            if(i=="d"):
-                text=i.replace('d', 'hxw')
-                sys.stdout.write(text)
-            if(i=='e'):
-                text=i.replace('e', 'kel')
-                sys.stdout.write(text)
-            if(i=='f'):
-                text=i.replace('f', 'wcg')
-                sys.stdout.write(text)
-            if(i=='g'):
-                text=i.replace('g', 'qjr')
-                sys.stdout.write(text)
-            if(i=="h"):
-                text=i.replace('h', 'opz')
-                sys.stdout.write(text)
-            if(i=="i"):
-                text=i.replace('i', 'msa')
-                sys.stdout.write(text)
-            if(i=="j"):
-                text=i.replace('j', 'erb')
-                sys.stdout.write(text)
-            if(i=="k"):
-                text=i.replace('k', 'cui')
-                sys.stdout.write(text)
-            if(i=="l"):
-                text=i.replace('l', 'dot')
-                sys.stdout.write(text)
-            if(i=="m"):
-                text=i.replace('m', 'rpe')
-                sys.stdout.write(text)
-            if(i=="n"):
-                text=i.replace('n', 'uyx')
-                sys.stdout.write(text)                                                                                                 
-            if(i=="o"):
-                text=i.replace('o', 'ncz')
-                sys.stdout.write(text)
-            if(i=="p"):
-                text=i.replace('p', 'tfm')
-                sys.stdout.write(text)
-            if(i=="q"):
-                text=i.replace('q', 'htq')
-                sys.stdout.write(text)
-            if(i=="r"):
-                text=i.replace('r', 'hlp')
-                sys.stdout.write(text)
-            if(i=="s"):
-                text=i.replace('s', 'wzf')
-                sys.stdout.write(text)
-            if(i=="t"):
-                text=i.replace('t', 'zua')
-                sys.stdout.write(text)
-            if(i=="u"):
-                text=i.replace('u', 'bak')
-                sys.stdout.write(text)
-            if(i=="v"):
-                text=i.replace('v', 'drk')
-                sys.stdout.write(text)
-            if(i=="w"):
-                text=i.replace('w', 'she')
-                sys.stdout.write(text)
-            if(i=="x"):
-                text=i.replace('x', 'foq')
-                sys.stdout.write(text)
-            if(i=="y"):
-                text=i.replace('y', 'dor')
-                sys.stdout.write(text)
-            if(i=="z"):
-                text=i.replace('z', 'imd')
-                sys.stdout.write(write)
-            if(i=="A"):
-                text=i.replace('A', 'cDa')
-                sys.stdout.write(text)
-            if(i=="B"):
-                text=i.replace('B', 'FbA')
-                sys.stdout.write(text)
-            if(i=="C"):
-                text=i.replace('C', 'Dvn')
-                sys.stdout.write(text)
-            if(i=="D"):
-                text=i.replace('D', 'hxW')
-                sys.stdout.write(text)
-            if(i=="E"):
-                text=i.replace('E', 'Kel')
-                sys.stdout.write(text)
-            if(i=="F"):
-                text=i.replace('F', 'wCG')
-                sys.stdout.write(text)
-            if(i=="G"):
-                text=i.replace('G', 'QJr')
-                sys.stdout.write(text)
-            if(i=="H"):
-                text=i.replace('H', 'OPZ')
-                sys.stdout.write(text)
-            if(i=="I"):
-                text=i.replace('I', 'MsA')
-                sys.stdout.write(text)
-            if(i=="J"):
-                text=i.replace('J', 'eRb')
-                sys.stdout.write(text)
-            if(i=="K"):
-                text=i.replace('K', 'CUi')
-                sys.stdout.write(text)
-            if(i=="L"):
-                text=i.replace('L', 'DOT')
-                sys.stdout.write(text)
-            if(i=="M"):
-                text=i.replace('M', 'RPe')
-                sys.stdout,write(text)
-            if(i=="N"):
-                text=i.replace('N', 'Uyx')
-                sys.stdout.write(text)
-            if(i=="O"):
-                text=i.replace('O', 'Ncz')
-                sys.stdout.write(text)
-            if(i=="P"):
-                text=i.replace('P', 'TFm')
-                sys.stdout.write(text)
-            if(i=="Q"):
-                text=i.replace('Q', 'htQ')
-                sys.stdout.write(text)
-            if(i=="R"):
-                text=i.replace('R', 'hLP')
-                sys.stdout.write(text)
-            if(i=="S"):
-                text=i.replace('S', 'wZf')
-                sys.stdout.write(text)
-            if(i=="T"):
-                text=i.replace('T', 'ZUA')
-                sys.stdout.write(text)
-            if(i=="U"):
-                text=i.replace('U', 'BaK')
-                sys.stdout.write(text)
-            if(i=="V"):
-                text=i.replace('V', 'dRk')
-                sys.stdout.write(text)
-            if(i=="W"):
-                text=i.replace('W', 'She')
-                sys.stdout.write(text)
-            if(i=="X"):
-                text=i.replace('X', 'FOq')
-                sys.stdout.write(text)
-            if(i=="Y"):
-                text=i.replace('Y', 'doR')
-                sys.stdout.write(text)
-            if(i=="Z"):
-                text=i.replace('Z', 'IMD')
-                sys.stdout.write(text)
-            if(i=='!'):
-                text=i.replace('!', '..@')
-                sys.stdout.write(text)
-            if(i=='@'):
-                text=i.replace('@', '[&8.')
-                sys.stdout.write(text)
-            if(i=='#'):
-                text=i.replace('#', '3[]')
-                sys.stdout.write(text)
-            if(i=='$'):
-                text=i.replace('$', '*g*')
-                sys.stdout.write(text)
-            if(i=='%'):
-                text=i.replace('%', '&qX')
-                sys.stdout.write(text)
-            if(i=='^'):
-                text=i.replace('^', '000')
-                sys.stdout.write(text)
-            if(i=='&'):
-                text=i.replace('&', 'AAA')
-                sys.stdout.write(text)
-            if(i=='*'):
-                text=i.replace('*', '5d:')
-                sys.stdout.write(text)
-            if(i=='('):
-                text=i.replace('(', 'z11')
-                sys.stdout.write(text)
-            if(i==')'):
-                text=i.replace(')', '235')
-                sys.stdout.write(text)
-            if(i=='-'):
-                text=i.replace('-', 'n4-')
-                sys.stdout.write(text)
-            if(i=='_'):
-                text=i.replace('_', '9n3')
-                sys.stdout.write(text)
-            if(i=='='):
-                text=i.replace('=', '-=-')
-                sys.stdout.write(text)
-            if(i=='+'):
-                text=i.replace('+', '#i#')
-                sys.stdout.write(text)
-            if(i=='['):
-                text=i.replace('[', '(s)')
-                sys.stdout.write(text)
-            if(i=='{'):
-                text=i.replace('{', '888')
-                sys.stdout.write(text)
-            if(i==']'):
-                text=i.replace(']', '456')
-                sys.stdout.write(text)
-            if(i=='}'):
-                text=i.replace('}', '8gh')
-                sys.stdout.write(text)
-            if(i==';'):
-                text=i.replace(';', 'p.,')
-                sys.stdout.write(text)
-            if(i==':'):
-                text=i.replace(':', '^r*')
-                sys.stdout.write(text)
-            if(i=="'"):
-                text=i.replace("'", '$3d')
-                sys.stdout.write(text)
-            if(i=='"'):
-                text=i.replace('"', '+Zl')
-                sys.stdout.write(text)
-            if(i=='|'):
-                text=i.replace('|', 'J.&')
-                sys.stdout.write(text)
-            if(i=='<'):
-                text=i.replace('<', '5U{')
-                sys.stdout.write(text)
-            if(i==','):
-                text=i.replace(',', '_S,')
-                sys.stdout.write(text)
-            if(i=='>'):
-                text=i.replace('>', '!8+')
-                sys.stdout.write(text)
-            if(i=='.'):
-                text=i.replace('.', 'T??')
-                sys.stdout.write(text)
-            if(i=='/'):
-                text=i.replace('/', 'Z^^')
-                sys.stdout.write(text)
-            if(i=='?'):
-                text=i.replace('?', ';R;')
-                sys.stdout.write(text)
-            if(i==' '):
-                text=i.replace(' ', '9sb')
-                sys.stdout.write(text)
-            if(i=='0'):
-               text=i.replace('0','2zz')
-               sys.stdout.write(text)
-            if(i=='1'):
-               text=i.replace('1','3Tc')
-               sys.stdout.write(text)
-            if(i=='2'):
-               text=i.replace('2','4z0')
-               sys.stdout.write(text)
-            if(i=='3'):
-               text=i.replace('3','5-R')
-               sys.stdout.write(text)
-            if(i=='4'):
-               text=i.replace('4','6sW')
-               sys.stdout.write(text)
-            if(i=='5'):
-               text=i.replace('5','7PR')
-               sys.stdout.write(text)
-            if(i=='6'):
-               text=i.replace('6','89F')
-               sys.stdout.write(text)
-            if(i=='7'):
-               text=i.replace('7','997')
-               sys.stdout.write(text)
-            if(i=='8'):
-               text=i.replace('8','10d')
-               sys.stdout.write(text)
-            if(i=='9'):
-               text=i.replace('9','11J')
-               sys.stdout.write(text)
-    if (encryption=="d" or encryption=="D"):
-        print "Type text  for decryption"
-        decryption_text=raw_input()
-        text=decryption_text.replace('cda', 'a').replace('fba', 'b').replace('dvn', 'c').replace('hxw', 'd').replace('kel', 'e').replace('wcg', 'f').replace('qjr', 'g').replace('opz','h').replace( 'msa', 'i').replace('erb', 'j').replace('cui',  'k').replace('dot', 'l').replace('rpe', 'm').replace('uyx', 'n').replace( 'ncz', 'o').replace('tfm', 'p').replace('htq', 'q').replace('hlp', 'r').replace('wzf', 's') .replace('zua', 't').replace('bak', 'u').replace('drk', 'v').replace('she', 'w').replace('foq', 'x').replace('dor', 'y').replace('imd', 'z').replace('cDa', 'A').replace('FbA', 'B').replace('Dvn', 'C').replace('hxW', 'D').replace('Kel', 'E').replace('wCG', 'F').replace('QJr', 'G').replace('OPZ', 'H').replace( 'MsA', 'T').replace( 'eRb', 'J').replace('CUi', 'K').replace('DOT', 'L').replace('RPe', 'M').replace('Uyx', 'N').replace('Ncz', 'O').replace('TFm', 'P').replace('htQ', 'Q').replace('hLP', 'R').replace('wZf', 'S').replace('ZUA', 'T').replace('BaK', 'U').replace('dRk', 'V').replace('She', 'W').replace('FOq', 'X').replace('doR', 'Y').replace('IMD', 'Z').replace('..@', '!').replace('[&8.', '@').replace('3[]', '#').replace( '*g*', '$').replace('&qX', '%').replace('000', '^').replace('AAA', '&').replace('5d:', '*').replace('z11', '(').replace('235', ')').replace('n4-', '-').replace('9n3', '_').replace('-=-', '=').replace('#i#', '+').replace('(s)', '[').replace('888', '{').replace('456', ']').replace('8gh', '}').replace('p.,', ';').replace('^r*', ':').replace('$3d', "'").replace('+Zl', '"').replace('J.&', '|').replace('5U{', '<').replace('_S,', ',').replace('!8+', '>').replace('T??', '.').replace('Z^^', '/').replace(';R;', '?').replace('9sb', ' ').replace('2zz','0').replace('3Tc','1').replace('4z0','2').replace('5-R','3').replace('6sW','4').replace('7PR','5').replace('89F','6').replace('997','7').replace('10d','8').replace('11J','9')
-       sys.stdout.write(text)
+print("Για αποκρυπτογράφηση πατήστε 'd'")
+
+choice = input("Επιλογή: ").strip().lower()
+
+while choice not in {'e', 'd'}:
+    print("Λανθασμένη επιλογή.")
+    choice = input("Επιλογή: ").strip().lower()
+
+if choice == 'e':
+    text = input("Πληκτρολογήστε το κείμενο για κρυπτογράφηση: ")
+    encrypted_text = encrypt(text)
+    print(f"Κρυπτογραφημένο κείμενο: {encrypted_text}")
+elif choice == 'd':
+    text = input("Πληκτρολογήστε το κείμενο για αποκρυπτογράφηση: ")
+    decrypted_text = decrypt(text)
+    print(f"Αποκρυπτογραφημένο κείμενο: {decrypted_text}")
 
 
